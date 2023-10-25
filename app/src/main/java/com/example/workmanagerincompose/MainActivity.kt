c5d331e511da790abebaaf405bc0b0527c274ff5
Mohamad <mohamad.alemicode@gmail.com>
2023-10-25 13:47:44 +0330

    Add DownloadWorker.kt

--- app/src/main/java/com/example/workmanagerincompose/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerincompose/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import com.example.workmanagerincompose.ui.theme.WorkManagerInComposeTheme
 
