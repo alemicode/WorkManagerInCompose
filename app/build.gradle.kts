54d1c86ff8a4e1053626b2b7d43fedfc4e31335f
Mohamad <mohamad.alemicode@gmail.com>
2023-10-26 10:18:55 +0330

    Call work manager from MainActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.workmanagerincompose"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.workmanagerincompose"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 30
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/workmanagerincompose/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerincompose/MainActivity.kt
@@ -1,33 +1,122 @@
 package com.example.workmanagerincompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.work.Constraints
+import androidx.work.ExistingWorkPolicy
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import coil.compose.rememberImagePainter
 import com.example.workmanagerincompose.ui.theme.WorkManagerInComposeTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState", "RememberReturnType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val downloadRequest = OneTimeWorkRequestBuilder<DownloadWorker>()
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(
+                        NetworkType.CONNECTED
+                    ).build()
+            ).build()
+        val colorFilterRequest = OneTimeWorkRequestBuilder<ColorFilterWorker>().build()
+        val workManager = WorkManager.getInstance(applicationContext)
 
         setContent {
             WorkManagerInComposeTheme {
+                val workInfo = workManager
+                    .getWorkInfosForUniqueWorkLiveData("download")
+                    .observeAsState()
+                    .value
+                val downloadInfo = remember(workInfo) {
+                    workInfo?.find { it.id == downloadRequest.id }
+                }
+
+                val colorFilterInfo = remember(workInfo) {
+                    workInfo?.find { it.id == colorFilterRequest.id }
+                }
+
+                val imageUri by derivedStateOf {
+                    val downloadUri =
+                        downloadInfo?.outputData?.getString(WorkerKeys.IMAGE_URI)?.toUri()
+
+                    val filterUri =
+                        colorFilterInfo?.outputData?.getString(WorkerKeys.FILTER_URI)?.toUri()
+                    filterUri ?: downloadUri
+                }
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    imageUri?.let { uri ->
+                        Image(
+                            painter = rememberImagePainter(
+                                data = uri
+                            ),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
+                    Button(onClick = {
+                        workManager.beginUniqueWork(
+                            "download",
+                            ExistingWorkPolicy.REPLACE,
+                            downloadRequest
+                        ).then(colorFilterRequest)
+                            .enqueue()
+                    }) {
+
+                        Text(text = "Click To Download")
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    when (downloadInfo?.state) {
+                        WorkInfo.State.RUNNING -> Text("Downloading...")
+                        WorkInfo.State.SUCCEEDED -> Text("Download succeeded")
+                        WorkInfo.State.FAILED -> Text("Download failed")
+                        WorkInfo.State.CANCELLED -> Text("Download cancelled")
+                        WorkInfo.State.ENQUEUED -> Text("Download enqueued")
+                        WorkInfo.State.BLOCKED -> Text("Download blocked")
+                        else -> {}
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    when (colorFilterInfo?.state) {
+                        WorkInfo.State.RUNNING -> Text("Applying filter...")
+                        WorkInfo.State.SUCCEEDED -> Text("Filter succeeded")
+                        WorkInfo.State.FAILED -> Text("Filter failed")
+                        WorkInfo.State.CANCELLED -> Text("Filter cancelled")
+                        WorkInfo.State.ENQUEUED -> Text("Filter enqueued")
+                        WorkInfo.State.BLOCKED -> Text("Filter blocked")
+                        else -> {}
+                    }
+
                 }
             }
         }
